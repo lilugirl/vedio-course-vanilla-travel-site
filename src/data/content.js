3a5d9062fde5e74b7694b78ac4dd243992f6848c
liu yi <lilugirl2005@126.com>
2022-06-19 13:00:16 +0800

    init

---
+++ src/data/content.js
@@ -0,0 +1,27 @@
+const CONTENT_INFO={
+    island:{
+        h1:"Never Stop",
+        h2:"Explore the world",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis delectus dolores sunt minima id dolorum placeat nemo atque, porro accusamus tempora praesentium! In modi voluptate magni laboriosam est, eveniet molestiae!"
+    },
+    spring:{
+        h1:"Spring",
+        h2:"Feel the new vitality",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis delectus dolores sunt minima id dolorum placeat nemo atque, porro accusamus tempora praesentium! In modi voluptate magni laboriosam est, eveniet molestiae!"
+    },
+    summer:{
+        h1:"Summer",
+        h2:"Enjoy passionate time",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis delectus dolores sunt minima id dolorum placeat nemo atque, porro accusamus tempora praesentium! In modi voluptate magni laboriosam est, eveniet molestiae!"
+    },
+    autumn:{
+        h1:"Autumn",
+        h2:"Experience without limits",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis delectus dolores sunt minima id dolorum placeat nemo atque, porro accusamus tempora praesentium! In modi voluptate magni laboriosam est, eveniet molestiae!"
+    },
+    winter:{
+        h1:"Winter",
+        h2:"Enjoy peace and freedom",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis delectus dolores sunt minima id dolorum placeat nemo atque, porro accusamus tempora praesentium! In modi voluptate magni laboriosam est, eveniet molestiae!"
+    }
+}
